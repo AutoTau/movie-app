c0395193dfe16ddb9275ec6b591e19fe74a93770
AutoTau <zenbenjam@gmail.com>
2021-04-15 23:09:16 -0700

    Added jumbotron display to movie display, displayed movie name with getMovieById()

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,21 +1,34 @@
 
 import { useRouter } from 'next/router'
+import { getMovieById } from '../../actions'
 
-
-
-
-const Movie = () => {
+const Movie = (props) => {
 
     const router = useRouter()
     const { id } = router.query
+    const { movie } = props
 
     return (
         <div className="container">
-            <h1>Movie with id: { id }</h1>
+          {/* <h1>Movie with id: { id }</h1> */}
+          <div class="jumbotron">
+              <h1 class="display-4">{movie.name}</h1>
+              <p class="lead">This is a simple hero unit, a simple jumbotron-style component</p>
+              <hr class="my-4" />
+              <p>It uses utility classes for typography and spacing to space content out</p>
+              <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </div>
+          <p>
+              Some description about the movie
+          </p>
         </div>
     )
+}
 
-
+// call getMovieById("2")
+Movie.getInitialProps = async () => {
+    const movie = await getMovieById("2")
+    return {movie}
 }
 
 export default Movie
