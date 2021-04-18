29f1c94dc171a04dc795ef315fb5dc988f626c26
AutoTau <zenbenjam@gmail.com>
2021-04-18 00:12:30 -0700

    Started getting movies from server

--- actions/index.js
+++ actions/index.js
@@ -1,41 +1,9 @@
 
+import axios from 'axios'
 
+const BASE_URL = 'http://localhost:3000'
 
-const MOVIE_DATA = [
-  {
-    id: '1',
-    name: 'The Shawshank Redemption',
-    releaseYear: 1994,
-    description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-    longDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    rating: 4.8,
-    genre: 'drama',
-    image_url: 'https://m.media-amazon.com/images/M/MV5BNjQ2NDA3MDcxMF5BMl5BanBnXkFtZTgwMjE5NTU0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg',
-    cover: 'https://images.unsplash.com/photo-1579186523804-b04c38349d2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
-  },
-  {
-    id: '2',
-    name: 'The Dark Knight',
-    releaseYear: 2008,
-    description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-    longDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    rating: 4.7,
-    genre: 'action, crime, drama',
-    image_url: 'https://img.cinemablend.com/filter:scale/quill/c/3/8/0/f/4/c380f4f12cfeec19f0c40c6f57db188f2f98cca8.jpg?mw=600',
-    cover: 'https://images.unsplash.com/photo-1497124401559-3e75ec2ed794?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
-  },
-  {
-    id: '3',
-    name: 'Lord of the Rings',
-    releaseYear: 2004,
-    description: 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
-    longDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    rating: 4.9,
-    genre: 'adventure, drama, fantasy',
-    image_url: 'https://img.cinemablend.com/filter:scale/quill/0/f/5/2/a/6/0f52a6843a25c1a5c1f9a0c00548cad9e1d912e2.jpg?mw=600',
-    cover: 'https://images.unsplash.com/photo-1599957015834-02d074874b82?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
-  }
-]
+const MOVIE_DATA = []
 
 const CATEGORY_DATA = [
   {id: 'c-1', name: 'Drama'},
@@ -65,12 +33,7 @@ export const createMovie = (movie) => {
 }
 
 export const getMovies = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(MOVIE_DATA) 
-      reject('Cannot fetch movie data!')
-    }, 50)
-  })
+  return axios.get(`${BASE_URL}/api/v1/movies`).then(res => res.data)
 }
 
 export const getMovieById = (id) => {
