d6616a66f18b04a27d1e30dd41324895924bff69
AutoTau <zenbenjam@gmail.com>
2021-04-18 02:04:53 -0700

    Added functionality to delete movie, tested and working

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,38 +1,45 @@
-
 import { useRouter } from 'next/router'
-import { getMovieById } from '../../actions'
+import { getMovieById, deleteMovie } from '../../actions'
 
 const Movie = (props) => {
 
-    const router = useRouter()
-    const { id } = router.query
-    const { movie } = props
+  const router = useRouter()
+  const { id } = router.query
+  const { movie } = props
 
-    return (
-        <div className="container">
-          <div className="jumbotron">
-              <h1 className="display-4">{movie.name}</h1>
-              <p className="lead">{ movie.description }</p>
-              <hr className="my-4" />
-              <p>{ movie.genre }</p>
-              <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-          </div>
-          <p className="desc-text">
-            { movie.longDesc }
-          </p>
-          <style jsx>{`
+  const handleDeleteMovie = () => {
+    deleteMovie(id).then(() => {
+      //Redirect to home
+      router.push('/')
+    })
+  }
+
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4">{movie.name}</h1>
+        <p className="lead">{movie.description}</p>
+        <hr className="my-4" />
+        <p>{movie.genre}</p>
+        <button className="btn btn-primary btn-lg mr-2" href="#" role="button">Learn More</button>
+        <button onClick={() => handleDeleteMovie(id)} className="btn btn-danger btn-lg" href="#" role="button">Delete</button>
+      </div>
+      <p className="desc-text">
+        {movie.longDesc}
+      </p>
+      <style jsx>{`
             .desc-text {
                 font-size: 21px;
             }
           `}
-          </style>
-        </div>
-    )
+      </style>
+    </div>
+  )
 }
 
 Movie.getInitialProps = async ({ query }) => {
-    const movie = await getMovieById(query.id)
-    return {movie}
+  const movie = await getMovieById(query.id)
+  return { movie }
 }
 
 export default Movie
--- server/index.js
+++ server/index.js
@@ -45,7 +45,21 @@ app.prepare().then(() => {
 
     server.delete('/api/v1/movies/:id', (req, res) => {
         const { id } = req.params
-        return res.json({ message: `Deleting post of id: ${id}` })
+        
+        const movieIndex = moviesData.findIndex(m => m.id === id)
+
+        // Delete movie at movieIndex
+        moviesData.splice(movieIndex, 1)
+
+        const pathToFile = path.join(__dirname, filePath)
+        const stringifiedData = JSON.stringify(moviesData, null, 2)
+
+        fs.writeFile(pathToFile, stringifiedData, (err) => {
+            if (err) {
+                return res.status(422).send(err)
+            }
+            return res.json('Movie has been successfully added')
+        })
     })
 
     // we are handling all of the request comming to our server
