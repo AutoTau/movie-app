c743cc5004263745e3327d45248fa2597ee723b8
AutoTau <zenbenjam@gmail.com>
2021-04-18 03:06:31 -0700

    added edit button to movie pages, pre filled edit forms

--- components/movieCreateForm.js
+++ components/movieCreateForm.js
@@ -1,17 +1,30 @@
+import { useState, useEffect } from 'react'
 
-import { useState } from 'react'
 
 
 const MovieCreateform = (props) => {
 
-    const [form, setform] = useState({
+    const [isInitialDataLoaded, setIsInitialDataLoaded] = useState(false)
+
+    const defaultData = {
         name: '',
         description: '',
         rating: '',
         image: '',
         cover: '',
         longDesc: ''
-    })
+    }
+
+    const formData = props.initialData ? { ...props.initialData } : defaultData
+
+    const [form, setform] = useState(formData)
+
+    // useEffect(() => {
+    //   if (props.initialData) {
+    //     setform(props.initialData)
+    //     setIsInitialDataLoaded(true)
+    //   }
+    // }, [isInitialDataLoaded])
 
     const handleChange = (event) => {
         const target = event.target
