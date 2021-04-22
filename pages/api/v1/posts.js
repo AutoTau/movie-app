156303d2e63274aac8c691aa0c325e1935656788
AutoTau <zenbenjam@gmail.com>
2021-04-22 05:37:29 -0700

    Added POST endpoint functionality

--- pages/api/v1/posts.js
+++ pages/api/v1/posts.js
@@ -2,8 +2,19 @@ import axios from 'axios'
 
 
 export default async (req, res) => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    const posts = response.data
 
-    return res.json(posts.slice(0, 10))
+    if (req.method === 'POST') {
+        const postData = Json.parse(req.body)
+        console.log(postData)
+
+        return res.json({
+            status: 'Saving Post to DB',
+            ...postData
+        })
+    } else {
+        // In our case this would be a get request
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        const posts = response.data
+        return res.json(posts.slice(0, 20))
+    }
 }
